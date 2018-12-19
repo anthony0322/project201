5b3ec7b1a7aad78be8e4b4b8538ba5adde87e723
Harvey <44796273+payathought@users.noreply.github.com>
2018-12-19 18:32:17 +0800

    Update navbar.php

--- assets/include/navbar.php
+++ assets/include/navbar.php
@@ -1,29 +1,23 @@
- <nav class="navbar__wrap " id="js-header">
-
-    
-    <div class="navbar">
-
-        <div class="pull-left">
-                            <a @click.stop.prevent="showCategories = !(showCategories);" href="#"
-                   class="navbar__categories icon-icon-category">Categories</a>
-                        
-
+ <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+        <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>
         </div>
-        <div class="pull-right">
-            <a href="/submit" class="navbar__submit hidden-xs">Submit a project</a>
-
-                                                    <a data-toggle="tooltip" title="Follow Us On Twitter"  target="_blank" rel="external" href="http://twitter.com/madewithlaravel"
-                   class="navbar__twitter">
-                    <i class="fab fa-fw fa-twitter"></i>
-                </a>
-                                        <a data-toggle="tooltip" title="Join our Telegram Channel" target="_blank" rel="external" href="https://t.me/MadeWithLaravel"
-                   class="navbar__twitter">
-                    <i class="fab fa-fw fa-telegram-plane"></i>
-                </a>
-            
-        </div>
-        <a class="navbar__logo" href="https://madewithlaravel.com">
-                        <img src="https://madewithlaravel.com/mandant/madewithlaravel/images/logo.png" width="194px" alt="MadeWithLaravel.com"/>
-                    </a>
-    </div>
-</nav>
+      </div>
+    </nav>
