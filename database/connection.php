635af7e2375bc7dd59bc5760ede1cc59c8fc2901
Harvey <44796273+payathought@users.noreply.github.com>
2018-12-20 00:01:22 +0800

    d

--- database/connection.php
+++ database/connection.php
@@ -1,28 +1,28 @@
 <?php 
 
-$dsn = "mysql:host=localhost;dbname=project201";
+$dsn = "mysql:host=localhost";
 $username = "root";
 $pass = "";
 try{
 $conn = new PDO($dsn,$username,$pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-if($conn->select_db('project201') === false){
+if($conn->query('use project201') === false){
 	$sql = "CREATE DATABASE project201";
     $conn->exec($sql);
     echo "Database created successfully<br>";
     }
-}
- $sql = "CREATE TABLE MyGuests (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    reg_date TIMESTAMP
-    )";
+
+ // $sql = "CREATE TABLE MyGuests (
+ //    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+ //    firstname VARCHAR(30) NOT NULL,
+ //    lastname VARCHAR(30) NOT NULL,
+ //    email VARCHAR(50),
+ //    reg_date TIMESTAMP
+ //    )";
 
     // use exec() because no results are returned
-    $conn->exec($sql);
-    echo "Table MyGuests created successfully";
+    // $conn->exec($sql);
+    // echo "Table MyGuests created successfully";
     }
 catch(PDOException $e)
     {
