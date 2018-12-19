0d682bc123cc59d60e3d67088724cb70d57e7ffb
anthony0322 <44447137+anthony0322@users.noreply.github.com>
2018-12-19 19:10:24 +0800

    updated

--- index.php
+++
@@ -1,6 +0,0 @@
-<h1>Welcome<h1>
-<?php
-
-include_once 'assets/include/navbar.php';
-
-?>
---
+++ view/index.php
@@ -0,0 +1,25 @@
+<?php 
+include '../assets/include/navbar.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/bootstrap.css'>
+<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/bootstrap.min.css'>
+<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font.css'>
+<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font-second.css'>
+<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font-third.css'>
+<link rel='stylesheet' type='text/css' href='../assets/css/custom.css'>
+<script src="../assets/js/bootstrap.js"></script>
+<script src="../assets/js/bootstrap-bundle.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+
+
+
+</head>
+<body>	
+<h1>Welcome to our site.</h1>
+</body>
+</html>
