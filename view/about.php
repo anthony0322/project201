fabec2fc887dad477a16a4c4f996b7ff76418032
anthony0322 <44447137+anthony0322@users.noreply.github.com>
2018-12-19 20:14:46 +0800

    Update about.php

--- view/about.php
+++ view/about.php
@@ -3,14 +3,14 @@
 <head>
 	<title>About</title>
 	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/bootstrap.css'>
-<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/bootstrap.min.css'>
-<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font.css'>
-<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font-second.css'>
-<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font-third.css'>
-<link rel='stylesheet' type='text/css' href='../assets/css/custom.css'>
-<script src="../assets/js/bootstrap.js"></script>
-<script src="../assets/js/bootstrap-bundle.js"></script>
-<script src="../assets/js/bootstrap.min.js"></script>
+	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/bootstrap.min.css'>
+	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font.css'>
+	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font-second.css'>
+	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font-third.css'>
+	<link rel='stylesheet' type='text/css' href='../assets/css/custom.css'>
+	<script src="../assets/js/bootstrap.js"></script>
+	<script src="../assets/js/bootstrap-bundle.js"></script>
+	<script src="../assets/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php include '../assets/include/navbar.php'; ?>
