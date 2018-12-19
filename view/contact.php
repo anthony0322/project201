f022afce8a207be09691f5ef3f2936a8bc530070
anthony0322 <44447137+anthony0322@users.noreply.github.com>
2018-12-19 22:04:31 +0800

    d

--- view/contact.php
+++ view/contact.php
@@ -13,9 +13,30 @@
 <script src="../assets/js/bootstrap-bundle.js"></script>
 <script src="../assets/js/bootstrap.min.js"></script>
 <body>
-	
-	<form>
+	<div class="col-lg-4">
 		
-	</form>
+	</div>
+	<div class="col-lg-4 form-div">
+		<form>
+			<div class="form-group">
+		    <label>Name</label>
+		    <input type="text" class="form-control"  aria-describedby="emailHelp" placeholder="Enter your name">
+		  </div>
+		  <div class="form-group">
+		    <label>Email address</label>
+		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputPassword1">Message</label>
+		    <textarea class="form-control" placeholder="message"></textarea>
+		  </div>
+		  <button type="submit" class="btn btn-primary">Send</button>
+		</form>
+	</div>	
+	<div class="col-lg-4">
+		
+	</div>
+
+
 </body>
 </html>
