5b0fad95e7bd0cd1f44a10e85d16c8a2d2b64519
anthony0322 <44447137+anthony0322@users.noreply.github.com>
2018-12-19 21:38:38 +0800

    d

--- view/home.php
+++ view/home.php
@@ -3,6 +3,12 @@
 <html>
 <head>
 	<title>Home</title>
+		<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/bootstrap.css'>
+	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/bootstrap.min.css'>
+	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font.css'>
+	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font-second.css'>
+	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font-third.css'>
+	<link rel='stylesheet' type='text/css' href='../assets/css/custom.css'>
 <script src="../assets/js/bootstrap.js"></script>
 <script src="../assets/js/bootstrap-bundle.js"></script>
 <script src="../assets/js/bootstrap.min.js"></script>
