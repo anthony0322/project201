4de9c26e6d9a36a3acd831147da607392b459007
anthony0322 <44447137+anthony0322@users.noreply.github.com>
2018-12-20 15:20:34 +0800

    delete messages

--- controller/contactController.php
+++ controller/contactController.php
@@ -1,7 +1,14 @@
 <?php 
+
 if(isset($_POST['send'])){
 	insertMessage($_POST['senderName'],$_POST['senderEmail'],$_POST['senderMessage']);
 }
+
+if (isset($_GET['delete'])) {
+	deleteMessage($_GET['id']);
+}
+
+
 function insertMessage($name, $email, $message){
 include '../database/connection.php';
 $data =[
@@ -28,6 +35,17 @@ $statement->execute();
 $messages = $statement->fetchAll();
 
 }
+
+
+function deleteMessage($id){
+include '../database/connection.php';
+
+$query = "DELETE FROM messages WHERE id=".$id;
+$statement = $conn->prepare($query);
+$statement->execute();
+	header("location: ../view/messages.php");
+}
+
 
 
 
--- view/messages.php
+++ view/messages.php
@@ -7,8 +7,8 @@
 		<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/bootstrap.css'>
 	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/bootstrap.min.css'>
 	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font.css'>
-	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font-second.css'>
-	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/font-third.css'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel='stylesheet' type='text/css' href='../assets/css/bootstrap/footer-css.css'>
 	<link rel='stylesheet' type='text/css' href='../assets/css/custom.css'>
 <script src="../assets/js/bootstrap.js"></script>
 <script src="../assets/js/bootstrap-bundle.js"></script>
@@ -18,22 +18,24 @@
 
 	<div class="container">
 		<div class="row">
-			<div class="col-md-8">
-				<div class="form-group">
+			<!-- <div class="col-md-12"> -->
+				<!-- <div class="form-group"> -->
 					<br>
 					<?php 
 					include '../controller/contactController.php';
 					getMessage($messages);
 						foreach($messages as $message){
+							echo "<div class='col-md-8 msg'>";
 							echo "<label class='form-control'>Name:".$message['sender_name']."
 							<br>Email:".$message['email']."
 							<br>Message:".$message['message']."
-							<br>Time sent:".date('M d, l | h:i:s', strtotime($message['time_sent']))."</label>";
+							<br>Time sent:".date('M d, l | h:i:s', strtotime($message['time_sent']))."</label></div>
+							<div class='col-md-4'><a href='../controller/contactController.php?id=".$message['id']."&delete='><span class='fa fa-trash msgmargin'></span></a></div>";
 						
 						}
 					 ?>
-				</div>
-			</div>
+				<!-- </div> -->
+			<!-- </div> -->
 		</div>
 	</div>
 
